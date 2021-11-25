804a598f71da24dc64d1d9d6e378de38fd443cd0
Martin Vrba <martin.vrba@pslib.cz>
2021-11-25 09:19:57 +0100

    Přidejte soubory projektu.

---
+++ MemoryCounter/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace MemoryCounter
+{
+	class SpojovySeznam<T>
+	{
+		Uzel zacatek, konec;
+		public int Count { get; private set; }
+		
+		/*public T this[int index]
+		{
+			
+		}*/
+
+
+		class Uzel
+		{
+			public T data;
+			public Uzel dalsi, predchozi;
+		}
+
+		public void VlozNaZacatek(T vstup)
+		{
+			Uzel novy = new Uzel();
+			novy.data = vstup;
+			novy.dalsi = zacatek;
+			zacatek = novy;
+
+			Count++;
+		}
+		public void VlozNaKonec(T vstup)
+		{
+			Uzel novy = new Uzel();
+			novy.data = vstup;
+			novy.predchozi = konec;
+			konec = novy;
+
+			Count++;
+		}
+
+		public void VypsatPrvky()
+		{
+			Uzel docasny = zacatek;
+			if (zacatek == null) return;
+			
+			while (docasny.dalsi != null)
+			{
+				Console.WriteLine(docasny.data);
+				docasny = docasny.dalsi;
+			}
+				Console.WriteLine(docasny.data);
+			
+			
+		}
+	}
+
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			SpojovySeznam<int> seznam = new SpojovySeznam<int>();
+			seznam.VlozNaZacatek(1);
+			seznam.VlozNaKonec(10);
+			seznam.VlozNaKonec(11);
+
+			seznam.VypsatPrvky();
+		}
+	}
+}
